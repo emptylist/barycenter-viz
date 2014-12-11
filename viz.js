712c7aebe364a1b93982314f11da4bd1a580a43b
emptylist <crooks1379@gmail.com>
2014-12-11 14:33:24 -0600

    Still need to work on rotations.

--- viz.js
+++ viz.js
@@ -1,36 +1,71 @@
 var lineSize = 400;
 var svg = d3.select("svg");
 var drag = d3.behavior.drag();
+var transformVelocityCoeff = 0.2;
+
+var v1 = {
+  x: 0,
+  y: 0
+};
+
+var v2 = {
+  x: 400,
+  y: 0,
+};
+
+var v3 = {
+  x: 200,
+  y: Math.sqrt(3) * 200,
+};
+
+var centroid = {
+  x: function () { return (1/3) * (v1.x + v2.x + v3.x); },
+  y: function () { return (1/3) * (v1.y + v2.y + v3.y) }
+};
+
+function translateSystem (dx, dy) {
+  v1.x += dx;
+  v1.y += dy;
+  v2.x += dx;
+  v2.y += dy;
+  v3.x += dx;
+  v3.y += dy;
+};
+
+translateSystem(100, 150);
 
 svg.select("#line1")
-  .attr("x1", 0)
-  .attr("y1", 0)
-  .attr("x2", lineSize)
-  .attr("y2", 0);
+  .attr("x1", v1.x)
+  .attr("y1", v1.y)
+  .attr("x2", v2.x)
+  .attr("y2", v2.y);
 
 svg.select("#line2")
-  .attr("x1", 0)
-  .attr("y1", 0)
-  .attr("x2", lineSize / 2)
-  .attr("y2", Math.sqrt(3) * lineSize / 2);
+  .attr("x1", v2.x)
+  .attr("y1", v2.y)
+  .attr("x2", v3.x)
+  .attr("y2", v3.y);
 
 svg.select("#line3")
-  .attr("x1", lineSize)
-  .attr("y1", 0)
-  .attr("x2", lineSize / 2)
-  .attr("y2", Math.sqrt(3)*lineSize / 2);
+  .attr("x1", v1.x)
+  .attr("y1", v1.y)
+  .attr("x2", v3.x)
+  .attr("y2", v3.y);
+
+svg.call(drag);
 
 var deg = 0;
 
 function update() {
-  deg += 0.5;
-  deg = deg % 360;
+  //deg += 0.5;
+  //deg = deg % 360;
   svg.select("g.axes")
-    .attr("transform", "rotate(" + deg + "," + lineSize/2 + "," + (1/3) * (Math.sqrt(3)*lineSize/2) + ")" +
-         "translate(100,100)");
+    .transition()
+    .duration(5)
+    .attr("transform", "rotate(" + deg + "," + centroid.x() + "," + centroid.y() + ")" );
 };
 
-setInterval(update, 20);
+//setInterval(update, 40);
 /*
 svg.selectAll("circle")
 .data([{x:10, y:10}])
@@ -45,13 +80,20 @@ svg.selectAll("circle").remove();
 */
 
 drag.on("dragstart", function (d) {
-  d3.select(this).classed("dragging", true);
+  d3.select(this).select("g.axes").classed("dragging", true);
 });
 
 drag.on("drag", function (d) {
-  d3.select(this).attr("cx", d.x += d3.event.dx).attr("cy", d.y += d3.event.dy);
+  var dx = d3.event.dx;
+  var dy = d3.event.dy;
+  var dThetaMag = transformVelocityCoeff * Math.sqrt(dx*dx + dy*dy);
+  var dThetaSign = 1;
+  deg = deg + (dThetaMag * dThetaSign) % 360;
+  update();
+  console.log(dx + "," + dy);
+//  d3.select(this).attr("cx", d.x += d3.event.dx).attr("cy", d.y += d3.event.dy);
 });
 
 drag.on("dragend", function (d) {
-  d3.select(this).classed("dragging", false);
+  d3.select(this).select("g.axes").classed("dragging", false);
 });
